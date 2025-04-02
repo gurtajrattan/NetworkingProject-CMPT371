94f6781006c4c4c8f01840765cd13fff311b8767
yadhuuu1110 <yadhuchoudhary.yc@gmail.com>
2025-04-01 21:47:56 -0700

    Update tcp_client.py

--- Yadhu/tcp_client.py
+++ Yadhu/tcp_client.py
@@ -1,1 +1,15 @@
+import socket
 
+def run_tcp_client(server_ip):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_ip, 54321))
+    
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        print(data.decode().strip())
+    client_socket.close()
+
+if __name__ == "__main__":
+    run_tcp_client("127.0.0.1")
